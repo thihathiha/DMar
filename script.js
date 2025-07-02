070b04c3695948d757ccddde1db5147907f38b99
thihathiha <thihaaung122122@gmail.com>
2025-07-03 01:59:42 +0400

    test

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function discoverfun(){
     document.querySelector("#discover").style.display = "block";
     document.querySelector("#room").style.display = "none";
-    document.querySelector("#roomlink").style.color = "grey";
+    document.querySelector("#food").style.display = "none";
+    document.querySelector("#about").style.display = "none";
+    document.querySelector("#aboutlink").style.color = "#B2B2B2";
+    document.querySelector("#foodlink").style.color = "#B2B2B2";
+    document.querySelector("#roomlink").style.color = "#B2B2B2";
     document.querySelector("#discoverlink").style.color = "#000";
 }
 
@@ -10,8 +14,38 @@ document.querySelector("#discoverlink").onclick = discoverfun;
 function roomfun(){
     document.querySelector("#discover").style.display = "none";
     document.querySelector("#room").style.display = "block";
+    document.querySelector("#food").style.display = "none";
+     document.querySelector("#about").style.display = "none";
+    document.querySelector("#aboutlink").style.color = "#B2B2B2";
+    document.querySelector("#foodlink").style.color = "#B2B2B2";
     document.querySelector("#roomlink").style.color = "#000";
-    document.querySelector("#discoverlink").style.color = "grey";
+    document.querySelector("#discoverlink").style.color = "#B2B2B2";
 }
 
 document.querySelector("#roomlink").onclick = roomfun;
+
+function foodfun(){
+    document.querySelector("#discover").style.display = "none";
+    document.querySelector("#room").style.display = "none";
+    document.querySelector("#food").style.display = "block";
+     document.querySelector("#about").style.display = "none";
+    document.querySelector("#aboutlink").style.color = "#B2B2B2";
+    document.querySelector("#foodlink").style.color = "#000";
+    document.querySelector("#roomlink").style.color = "#B2B2B2";
+    document.querySelector("#discoverlink").style.color = "#B2B2B2";
+}
+
+document.querySelector("#foodlink").onclick = foodfun;
+
+function aboutfun(){
+    document.querySelector("#discover").style.display = "none";
+    document.querySelector("#room").style.display = "none";
+    document.querySelector("#food").style.display = "none";
+    document.querySelector("#about").style.display = "flex";
+    document.querySelector("#aboutlink").style.color = "#000";
+    document.querySelector("#foodlink").style.color = "#B2B2B2";
+    document.querySelector("#roomlink").style.color = "#B2B2B2";
+    document.querySelector("#discoverlink").style.color = "#B2B2B2";
+}
+
+document.querySelector("#aboutlink").onclick = aboutfun;
